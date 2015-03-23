640b1072bc3cf28a396ebe772cf5ac6e579c7cd2
Kalpesh Kalekar <kalpeshk9967016292@gmail.com>
2015-03-23 15:00:19 +0530

    Login Mackup

--- index.php
+++ index.php
@@ -1,38 +1,57 @@
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="CSS/index.css">
-<link href='http://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="CSS/def.css">
+<link href='http://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'><meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 <title>CT Exam</title>
 </head>
 
 <body>
-<div class="logo">
+
+<div class="container">
+
+<div class="row top-buffer">
+	<div class="col-md-4 col-xs-4 "></div>
+	<div class="col-md-4 col-xs-4 center"><img src="IMG/CTL.png" class="img-responsive center-block"></div>
+	<div class="col-md-4 col-xs-4"></div>
+</div>
 </div>
 
-<div class="exam">
-Login to start your exam.
-</div>
-<div class="error">
-
-
-
-</div>
-<div class="login">
-
-<form name="login" action="login.php" method="post">
-<div class="form">
-<div class="tbx">
-<input type="text" placeholder="Enter User name" name="username" required="">
-<br>
-<input type="password" placeholder="Enter Password" name="pass" required="">
+<div class="container">
+<div class="row">
+	<div class="col-md-4 col-xs-4 "></div>
+	<div class="col-md-4 col-xs-4 "><h1 style="text-align:center;">Login to continue</h1></div>
+	<div class="col-md-4 col-xs-4"></div>
 </div>
 
-<input div class="btn" type="submit" value="Student Login" name="sbtn">
-<input div class="abtn" type="submit" value="Admin Login" name="abtn">
-
+<form method="post" action="login.php">
+<div class="row ">
+	<div class="col-md-4 col-xs-4 "></div>
+	<div class="col-md-4 col-xs-4 well">
+		<input type="text" class="form-control" id="uname" required="" placeholder="Enter Username">
+		<br>
+		<input type="password" class="form-control" id="pass" required="" placeholder="Enter Password"><br>
+	</div>
+	<div class="col-md-4 col-xs-4"></div>
 </div>
+
+<div class="row txtbx">
+	<div class="col-md-4 col-xs-4 "></div>
+	<div class="col-md-2 col-xs-2 ">
+		<button type="submit" class="btn btn-primary btn-block"><div class="txtbx">Student Login</div></button>
+	</div>
+	<div class="col-md-2 col-xs-2 ">
+		<button type="submit" class="btn btn-info btn-block"><div class="txtbx">Admin Login</div></button>
+	</div>
+	<div class="col-md-4 col-xs-4"></div>
 </div>
 </form>
 </div>
+</div>
+<script src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 </body>
 </html>
