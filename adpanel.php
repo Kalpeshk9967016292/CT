86f3318b48155e646dab9b56f1876d0384e00367
Kalpesh Kalekar <kalpeshk9967016292@gmail.com>
2015-03-31 12:44:23 +0530

    adpanel

--- adpanel.php
+++ adpanel.php
@@ -9,12 +9,9 @@
 <title>Admin Panel</title>
 </head>
 
-<<<<<<< HEAD
-<body>
-=======
+
 <body bgcolor="cadetblue">
-<?php
->>>>>>> origin/master
+
 
 <div class="container-fluid">
 	<div class="row top-buffer well">
