fdf9fcb29e295d5c30a1d01919214f3a6f284c3e
shaileshrpj <shaileshrpj@gmail.com>
2015-04-03 10:56:39 +0530

    kkkk

--- exam.php
+++ exam.php
@@ -5,20 +5,22 @@ if($_SESSION['username'])
 {
 echo "<b>Welcome,</b>".$_SESSION['username']."!";
 include('connect.php');
-$result=mysql_query("SELECT question_id FROM `questions` where type='mc' order by rand() limit 5");
+$result=mysql_query("SELECT * FROM `questions` where type='mc' order by rand() limit 5");
 while($row=mysql_fetch_array($result))
 {
 	$qid = $row['question_id'];
-	mysql_query("INSERT INTO temp_table(username,question_id)VALUES('$user','$qid')");
-echo $qid."<br>";
+	$type = $row['type'];
+	mysql_query("INSERT INTO temp_table(username,question_id,type)VALUES('$user','$qid','$type')");
+//echo $qid."<br>";
 }
 echo "<hr>";
-$result1 = mysql_query("SELECT question_id FROM `questions` where type='tf' order by rand() limit 5");
+$result1 = mysql_query("SELECT * FROM `questions` where type='tf' order by rand() limit 5");
 while($row=mysql_fetch_array($result1))
 {
 	$qid1 = $row['question_id'];
-	mysql_query("INSERT INTO temp_table(username,question_id)VALUES('$user','$qid1')");
-echo $qid1."<br>";
+	$type1 = $row['type'];
+	mysql_query("INSERT INTO temp_table(username,question_id,type)VALUES('$user','$qid1','$type1')");
+//echo $qid1."<br>";
 }
 header ("location:startexam.php");
 }
