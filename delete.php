e46a61a8efeccc74f263fd1d3d614eb7d6c90300
shaileshrpj <shaileshrpj@gmail.com>
2015-04-08 00:06:24 +0530

    nothing

--- delete.php
+++ delete.php
@@ -9,7 +9,7 @@ $sql = mysql_query("DELETE FROM stud_info where stud_id=$id");
 $sql1 = mysql_query("DELETE FROM student_log where sid='$id'");
 
 if($sql && $sql1 == true){
-	$msg = '<div class="alert alert-warning">Student Deleted successfully</div>';
+	$msg = '<div class="alert alert-warning">Student Deleted successfully !</div>';
 	header('location:manage.php?msg='.$msg.'');
 }
 
